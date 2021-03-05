1e454fc4053a8149937dffdbbc9ab1f6ddec26e2
Thomas Paulus <thomas.paulus6@etu.univ-lorraine.fr>
2021-03-05 11:30:07 +0100

    Bsic templat v2

--- SampleApp/randomActivitiesViewController.swift
+++ SampleApp/randomActivitiesViewController.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+
+class randomActivitiesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+       struct Bored: Codable {
+           var activity: String
+           var type: String
+           var participants: Int
+           var price: Double
+           var link: String?
+           var key: String
+           var accessibility: Double
+        
+       }
+        
+        let urlString = "https://www.boredapi.com/api/activity"
+        if let url = URL(string: urlString)
+        {
+            URLSession.shared.dataTask(with: url) { data, res, err in
+                if let data = data {
+                    
+                    let decoder = JSONDecoder()
+                    if let json = try? decoder.decode(Bored.self, from: data) {
+                        print(json)
+                        print(json.activity)
+                        }
+                    }
+                }.resume()
+                
+        }
+        
+
+        // Prints "Durian"
+        
+        
+        
+        //print(product.activity)
+        
+    }
+
+    
+    
+}
