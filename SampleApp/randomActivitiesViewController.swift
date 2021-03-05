0a18c74598e45bd8a109313916457b8228a245d4
Thomas Paulus <thomas.paulus6@etu.univ-lorraine.fr>
2021-03-05 13:56:43 +0100

    réparation

--- SampleApp/randomActivitiesViewController.swift
+++ SampleApp/randomActivitiesViewController.swift
@@ -1,10 +1,3 @@
-//
-//  randomActivitiesViewController.swift
-//  SampleApp
-//
-//  Created by Thomas Paulus on 05/03/2021.
-//
-
 import Foundation
 
 import UIKit
@@ -13,12 +6,15 @@ import Alamofire
 
 
 class randomActivitiesViewController: UIViewController {
-    
+
+
+    //@IBOutlet weak var activityNom: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
+
+
        struct Bored: Codable {
            var activity: String
            var type: String
@@ -27,34 +23,32 @@ class randomActivitiesViewController: UIViewController {
            var link: String?
            var key: String
            var accessibility: Double
-        
+
        }
-        
+
         let urlString = "https://www.boredapi.com/api/activity"
         if let url = URL(string: urlString)
         {
             URLSession.shared.dataTask(with: url) { data, res, err in
                 if let data = data {
-                    
+
                     let decoder = JSONDecoder()
                     if let json = try? decoder.decode(Bored.self, from: data) {
                         print(json)
                         print(json.activity)
+
+                        DispatchQueue.main.async {
+                                   self.activityNom.text = json.type
+                               }
                         }
                     }
                 }.resume()
-                
-        }
-        
 
-        // Prints "Durian"
-        
-        
-        
-        //print(product.activity)
-        
+        }
+
+
+
     }
 
-    
-    
-}
+
+    }
