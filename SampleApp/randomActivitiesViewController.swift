0f8f6181736744bc920956889a8fd22a4161d703
Gauthier Schmitt <gauthier.schmitt2@etu.univ-lorraine.fr>
2021-03-06 11:04:16 +0100

    loading etc...

--- SampleApp/randomActivitiesViewController.swift
+++ SampleApp/randomActivitiesViewController.swift
@@ -16,12 +16,22 @@ class randomActivitiesViewController: UIViewController {
     @IBOutlet weak var pbPrice: UIProgressView!
     @IBOutlet weak var pbAccessibility: UIProgressView!
     @IBOutlet weak var imgGif: UIImageView!
+    @IBOutlet weak var imgErr: UIImageView!
     
     //let imageView = UIImageView()
     //imgGif.loadGif(name: "load")
     
+    override func loadView() {
+        super.loadView()
+        imgGif.loadGif(asset: "load")
+    }
+
+    
     override func viewDidLoad() {
+       
         self.title = "Try this !"
+        
+        
         
         super.viewDidLoad()
         
@@ -38,7 +48,7 @@ class randomActivitiesViewController: UIViewController {
 
 
        }
-
+        
         let urlString = sendData
         if self.sendData != "https://www.boredapi.com/api/activity?price=0.0&participants=3.00"
         {
@@ -59,6 +69,8 @@ class randomActivitiesViewController: UIViewController {
                             self.activityLink.text = json.link ?? " "
                             self.activityAccessibility.text = String(format: "%.1f", json.accessibility)
                             self.pbAccessibility.progress = (Float)(json.accessibility)
+                            sleep(2)
+                            self.imgGif.isHidden = true
                               }
                         }
                     }
@@ -67,12 +79,61 @@ class randomActivitiesViewController: UIViewController {
         }
         else
         {
-            print("Salut")
+            self.imgGif.isHidden = true
+            self.imgErr.isHidden = false
         }
 
 
 
     }
 
+    @IBAction func btnRetry(_ sender: Any) {
+        self.imgGif.isHidden = false
+        struct Bored: Codable {
+            var activity: String
+            var type: String
+            var participants: Int
+            var price: Double
+            var link: String?
+            var key: String
+            var accessibility: Double
 
+
+        }
+        
+        let urlString = sendData
+        if self.sendData != "https://www.boredapi.com/api/activity?price=0.0&participants=3.00"
+        {
+        if let url = URL(string: urlString)
+        {
+            URLSession.shared.dataTask(with: url) { data, res, err in
+                if let data = data {
+
+                    let decoder = JSONDecoder()
+                    if let json = try? decoder.decode(Bored.self, from: data) {
+                        print(json)
+                        print(json.activity)
+                        DispatchQueue.main.async {
+                            self.activityNom.text = json.activity
+                            self.activityType.text = "Type : " + json.type
+                            self.activityPrice.text = String(format: "%.1f", json.price)
+                            self.pbPrice.progress = (Float)(json.price)
+                            self.activityLink.text = json.link ?? " "
+                            self.activityAccessibility.text = String(format: "%.1f", json.accessibility)
+                            self.pbAccessibility.progress = (Float)(json.accessibility)
+                            sleep(2)
+                            self.imgGif.isHidden = true
+                              }
+                        }
+                    }
+                }.resume()
+            }
+        }
+        else
+        {
+            self.imgGif.isHidden = true
+            self.imgErr.isHidden = false
+        }
+    }
+    
     }
